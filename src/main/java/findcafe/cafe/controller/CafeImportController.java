4d7661361075f0c56a3df688115c0263448b4136
shion002 <shion002002@gmail.com>
2025-12-31 16:44:12 +0900

    202512311644

--- src/main/java/findcafe/cafe/controller/CafeImportController.java
+++ src/main/java/findcafe/cafe/controller/CafeImportController.java
@@ -2,10 +2,8 @@ package findcafe.cafe.controller;
 
 import findcafe.cafe.service.CafeImportService;
 import findcafe.cafe.service.KaKaoCafeFilterService;
-import findcafe.cafe.service.NaverPlaceFilterCafes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CafeImportController {
 
     private final CafeImportService cafeImportService;
-    private final NaverPlaceFilterCafes naverPlaceFilterCafes;
     private final KaKaoCafeFilterService kaKaoCafeFilterService;
 
     public ResponseEntity<String> importCafes() {
@@ -23,13 +20,4 @@ public class CafeImportController {
         return ResponseEntity.ok("카페 데이터 수집이 백그라운드에서 시작되었습니다.");
     }
 
-    public ResponseEntity<String> naverFilterCafe() throws InterruptedException {
-        naverPlaceFilterCafes.filterRegisterCafes();
-        return ResponseEntity.ok("카페 필터링 시작");
-    }
-
-    public ResponseEntity<String> filterKakaoCafe(){
-        kaKaoCafeFilterService.filterByKaKaoReviews();
-        return ResponseEntity.ok("카카오 필터링 비동기 시작");
-    }
 }
